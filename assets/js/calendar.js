a7d12aa8aa21010a7d63c12fdebd03bf74272b4a
luispaiva <contato@luispaiva.com.br>
2020-03-15 19:45:03 -0300

    Criação da função que limpa os campos ao clicar em alguma data do calendário.

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -2,7 +2,7 @@ jQuery(function($) {
 
     var dialog;
 
-    if ( $('#calendar').length ) {
+    if ( $("#calendar").length ) {
         getEnvets();
     }
 
@@ -16,24 +16,25 @@ jQuery(function($) {
     // Render Calendar
     function calendar(arrayEvents) {
 
-        $('#calendar').fullCalendar({
-            plugins: [ 'interaction' ],
-            defaultView: 'month',
+        $("#calendar").fullCalendar({
+            plugins: [ "interaction" ],
+            defaultView: "month",
             weekNumbers: true,
             locale: "pt-br",
             header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay,listMonth'
+                left: "prev,next today",
+                center: "title",
+                right: "month,agendaWeek,agendaDay,listMonth"
             },
             selectable: true,
             selectHelper: true,
             select: function(start, end, allDay) {
+                cleanFields();
                 $("#eventContent").dialog({
                     modal: true, 
-                    title: 'Novo evento', 
+                    title: "Novo evento", 
                     width:500,
-                    at: 'center',
+                    at: "center",
                     buttons: {
                         Cancel: function() {
                             $( this ).dialog( "close" );
@@ -48,20 +49,20 @@ jQuery(function($) {
             eventLimit: true,
             events: arrayEvents,
             eventRender: function (event, element) {              
-                element.attr('href', 'javascript:void(0);');
-
+                element.attr("href", "javascript:void(0);");
+                cleanFields();
                 element.click(function() {
                     $("#eventTitle").val(event.title);
-                    $("#startTime").val(moment(event.start).format('MMM Do h:mm A'));
-                    $("#endTime").val(moment(event.end).format('MMM Do h:mm A'));
+                    $("#startTime").val(moment(event.start).format("MMM Do h:mm A"));
+                    $("#endTime").val(moment(event.end).format("MMM Do h:mm A"));
                     $("#eventInfo").val(event.description);
-                    $("#eventLink").attr('href', event.url);
+                    $("#eventLink").attr("href", event.url);
 
                     $("#eventContent").dialog({
                         modal: true, 
                         title: event.title, 
                         width:500,
-                        at: 'center',
+                        at: "center",
                         buttons: {
                         Cancel: function() {
                             $( this ).dialog( "close" );
@@ -76,10 +77,18 @@ jQuery(function($) {
         });
     }
 
+    function cleanFields() {
+        $("#eventTitle").val("");
+        $("#startTime").val("");
+        $("#endTime").val("");
+        $("#eventInfo").val("");
+        $("#eventLink").attr("");
+    }
+
     $( ".datepicker" ).datepicker({
       showOtherMonths: true,
       selectOtherMonths: true,
-      dateFormat: 'dd-mm-yy'
+      dateFormat: "dd-mm-yy"
     });
 
 });
